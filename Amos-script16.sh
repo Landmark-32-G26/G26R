66c8cb495277b67b0cca93485011c7b52f14a229
Oluskb <134785739+Oluskb@users.noreply.github.com>
2023-09-11 20:37:23 -0600

    design page v1.4.6
    Added 1 line of code

--- Amos-script16.sh
+++ Amos-script16.sh
@@ -5,3 +5,4 @@ read -p "Enter last name: " lname
 read -p "Enter your age: " age
 echo "Welcome $fname $lname! Your current age is: $age."
 echo "Honestly speaking, $fname $lname, you're getting older already."
+echo "Work Completed"
